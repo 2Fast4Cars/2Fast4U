db5129aa2ed914f1683e9137c19a88b03d9ce112
AndersBlommaN <anders_blomkvist_89@live.se>
2013-10-01 12:41:57 +0200

    GET LOG BITCHES

--- FastCars/src/com/me/fastcars/FastCars.java
+++ FastCars/src/com/me/fastcars/FastCars.java
@@ -1,27 +1,12 @@
 package com.me.fastcars;
 
-import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
-import com.badlogic.gdx.graphics.GL10;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.Texture.TextureFilter;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.MathUtils;
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
-import com.badlogic.gdx.physics.box2d.World;
-import com.sun.xml.internal.ws.util.StringUtils;
 
 public class FastCars extends Game {
 	
 	@Override
 	public void create() {		
-		setScreen(new GameScreen());
+		setScreen(new MenuScene());
 	}
 	
 }
