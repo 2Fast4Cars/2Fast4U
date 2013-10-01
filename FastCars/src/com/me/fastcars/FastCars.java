669cb9be7874568d930f265bf80f378d2261467f
AndersBlommaN <anders_blomkvist_89@live.se>
2013-10-01 13:43:17 +0200

    provar

--- FastCars/src/com/me/fastcars/FastCars.java
+++ FastCars/src/com/me/fastcars/FastCars.java
@@ -9,6 +9,7 @@ public class FastCars extends Game {
 	@Override
 	public void create() {		
 <<<<<<< HEAD
+<<<<<<< HEAD
 		setScreen(new MenuScene(this));
 	}
 	
@@ -17,6 +18,9 @@ public class FastCars extends Game {
 		
 		setScreen(screen);
 		
+=======
+		setScreen(new GameScreen());
+>>>>>>> 42ba884f2de9eb70b63861adfd6a29b6bdff7267
 =======
 		setScreen(new GameScreen());
 >>>>>>> 42ba884f2de9eb70b63861adfd6a29b6bdff7267
