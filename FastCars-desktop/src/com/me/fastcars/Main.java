b679af46cffc3a163068cc44103e5053e3d4c49f
AndersBlommaN <anders_blomkvist_89@live.se>
2013-10-25 10:38:00 +0200

    fixat resize

--- FastCars-desktop/src/com/me/fastcars/Main.java
+++ FastCars-desktop/src/com/me/fastcars/Main.java
@@ -6,10 +6,11 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 public class Main {
 	public static void main(String[] args) {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
-		cfg.title = "FastCars v0.3";
+		cfg.title = "FastCars v1.0";
 		cfg.useGL20 = true;
 		cfg.width = 1000;
 		cfg.height = 600;
+		cfg.resizable = false;
 		
 		new LwjglApplication(new FastCars(), cfg);
 	}
